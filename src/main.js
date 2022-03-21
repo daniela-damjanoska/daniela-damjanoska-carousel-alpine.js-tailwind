327134441e4560ca13590b99f2f4c5a65cedeead
Daniela <daniela_damjanoska@yahoo.com>
2022-03-21 16:19:30 +0100

    carousel swiper solved (not desktop)

--- src/main.js
+++ src/main.js
@@ -1,14 +1,36 @@
+let touchstartX = 0;
+let touchendX = 0;
+
 function carouselData(slides) {
     return {
       slides,
       activeSlide: 1,
+      initHandleSwipe() {
+        const slider = document.getElementById('slider');
+
+        slider.addEventListener('touchstart', e => {
+          touchstartX = e.changedTouches[0].screenX
+        })
+        slider.addEventListener('touchend', e => {
+          touchendX = e.changedTouches[0].screenX
+          if (touchendX < touchstartX) {
+            this.activeSlide =
+            this.activeSlide === 1 ? this.slides.length : this.activeSlide - 1;
+           
+          }
+          if (touchendX > touchstartX) {
+            this.activeSlide =
+            this.activeSlide === this.slides.length ? 1 : this.activeSlide + 1;
+          }
+        })
+      },
       goToPrevious() {
         this.activeSlide =
-          this.activeSlide === 1 ? this.slides.length : this.activeSlide - 1;
+        this.activeSlide === 1 ? this.slides.length : this.activeSlide - 1;
       },
       goToNext() {
         this.activeSlide =
-          this.activeSlide === this.slides.length ? 1 : this.activeSlide + 1;
-      }
+        this.activeSlide === this.slides.length ? 1 : this.activeSlide + 1;
+      } 
     };
   }
