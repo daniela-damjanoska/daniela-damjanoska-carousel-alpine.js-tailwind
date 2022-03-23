f9973c3db3b0bf55a5c82a4eaa2ee89c1985dc5b
Daniela <daniela_damjanoska@yahoo.com>
2022-03-23 10:49:21 +0100

    working in progress

--- src/main.js
+++ src/main.js
@@ -8,6 +8,21 @@ function carouselData(slides) {
       initHandleSwipe() {
         const slider = document.getElementById('slider');
 
+        // slider.addEventListener('mouseup', e => {
+        //   touchendX = e.clientY
+        // })
+        // slider.addEventListener('mousedown', e => {
+        //   touchstartX = e.clientY
+        //   if (touchendX > touchstartX) {
+        //     this.activeSlide =
+        //     this.activeSlide === 1 ? this.slides.length : this.activeSlide - 1;
+           
+        //   }
+        //   if (touchendX < touchstartX) {
+        //     this.activeSlide =
+        //     this.activeSlide === this.slides.length ? 1 : this.activeSlide + 1;
+        //   }
+        // })
         slider.addEventListener('touchstart', e => {
           touchstartX = e.changedTouches[0].screenX
         })
